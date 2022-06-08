ac6d4cb57827f8d0bef9e517129ddf0d5eb09132
Tim Xu <oubleknot14@aol.com>
2022-06-08 22:33:25 +0000

    first commit

--- webhook.py
+++ webhook.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-webhook_url = 'http://127.0.0.1:5000/webhook'
+webhook_url = 'https://webhook.site/03107233-0f88-4d68-b2c9-91b644b9fbfb'
 
 data = { 'name': 'DevOps Journey', 
-         'Channel URL': 'test url' }
+         'Channel URL': 'test url5' }
 
 r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
